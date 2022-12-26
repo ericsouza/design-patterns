71e685da516d628cdebb58c48d314e5c6aca98b5
Eric Souza <ericsouza0801@gmail.com>
2022-12-26 10:23:35 -0300

    first commit

---
+++ src/test/java/com/ericsouza/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsouza.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
